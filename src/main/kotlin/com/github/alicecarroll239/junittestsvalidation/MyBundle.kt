e2957ad80d5be4442aa08a771bab674fb1f95cba
GitHub Action <action@github.com>
2021-08-16 13:10:20 +0000

    Template cleanup

--- src/main/kotlin/com/github/alicecarroll239/junittestsvalidation/MyBundle.kt
+++ src/main/kotlin/com/github/alicecarroll239/junittestsvalidation/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.alicecarroll239.junittestsvalidation
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/alicecarroll239/junittestsvalidation/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alicecarroll239.junittestsvalidation.services
+
+import com.github.alicecarroll239.junittestsvalidation.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
